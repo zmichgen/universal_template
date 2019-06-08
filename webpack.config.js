c8fe9329592fc95af2ae12b708b8c2c236fbea10
zmichgen <zmichgen@gmail.com>
2019-06-08 08:08:27 +0300

    change browserslist

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,10 @@ module.exports = {
       filename: 'index.html',
       favicon: './public/html-5.png',
     }),
-    new CopyPlug([{ from: './public', to: './' }]),
+    new CopyPlug([{
+      from: './public',
+      to: './',
+    }]),
     new MiniCssPlug({
       filename: '[name].css',
       chunkFilename: '[id].css',
@@ -33,82 +36,75 @@ module.exports = {
     contentBase: path.join(__dirname, './dist'),
   },
   module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
+    rules: [{
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        },
+      },
+    },
+    {
+      test: /\.(png|jpg|gif|svg|cur)$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          name: 'assets/[name].[ext]',
         },
       },
       {
-        test: /\.(png|jpg|gif|svg|cur)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'assets/[name].[ext]',
-            },
+        loader: 'image-webpack-loader',
+        options: {
+          mozjpeg: {
+            progressive: true,
+            quality: 65,
           },
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                progressive: true,
-                quality: 65,
-              },
-              optipng: {
-                enabled: true,
-              },
-              pngquant: {
-                quality: '65-90',
-                speed: 4,
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-            },
+          optipng: {
+            enabled: true,
           },
-        ],
+          pngquant: {
+            quality: '65-90',
+            speed: 4,
+          },
+          gifsicle: {
+            interlaced: false,
+          },
+        },
       },
-      {
-        test: /\.(ttf|woff|woff2|eot)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'fonts/[name].[ext]',
-            },
-          },
-        ],
-      },
+      ],
+    },
+    {
+      test: /\.(ttf|woff|woff2|eot)$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          name: 'fonts/[name].[ext]',
+        },
+      }],
+    },
 
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          {
-            loader: MiniCssPlug.loader,
-            options: {
-              publicPath: '../',
-            },
-          },
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: [
-                Autoprefixer({
-                  browsers: ['ie >= 8', 'last 4 version'],
-                }),
-              ],
-            },
-          },
-          'sass-loader',
-        ],
+    {
+      test: /\.(sa|sc|c)ss$/,
+      use: [{
+        loader: MiniCssPlug.loader,
+        options: {
+          publicPath: '../',
+        },
       },
+      'css-loader',
+      {
+        loader: 'postcss-loader',
+        options: {
+          plugins: [
+            Autoprefixer,
+          ],
+        },
+      },
+      'sass-loader',
+      ],
+    },
     ],
   },
 };
